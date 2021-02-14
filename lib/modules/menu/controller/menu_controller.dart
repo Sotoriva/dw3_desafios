2c3546da9781e999ace54e61b7766db0df9eb9ee
Sotoriva <joaosotoriva@hotmail.com>
2021-02-14 17:24:57 -0300

    commit

--- lib/modules/menu/controller/menu_controller.dart
+++ lib/modules/menu/controller/menu_controller.dart
@@ -10,21 +10,14 @@ part 'menu_controller.g.dart';
 @Injectable()
 class MenuController {
   List<Menu> menu = [
-    Menu(id: 1,name: 'Mussarela',price: 35.00),
-    Menu(id: 1,name: 'Calabreza',price: 35.00),
-    Menu(id: 1,name: 'Quatro Queijos',price: 35.00),
+    Menu(id: 1, name: 'Mussarela', price: 35.00),
+    Menu(id: 1, name: 'Calabreza', price: 35.00),
+    Menu(id: 1, name: 'Quatro Queijos', price: 35.00),
   ];
 
   @Route.get('/')
   Future<Response> findAll(Request request) async {
-    try {
-      return Response.ok(
-        jsonEncode(menu?.map((e) => e.toMap())?.toList() ?? []),
-      );
-    } catch (e) {
-      print(e);
-      return Response.internalServerError();
-    }
+    return Response.ok(jsonEncode(menu?.map((e) => e.toMap())?.toList() ?? []));
   }
 
   Router get router => _$MenuControllerRouter(this);
